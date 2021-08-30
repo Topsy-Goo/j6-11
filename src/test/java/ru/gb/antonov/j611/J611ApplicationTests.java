5c4a848d2d9728934b6ecfe1d0303ed0d76a7f56
Topsy-Goo <topsygoo@yandex.ru>
2021-08-30 21:37:13 +0300

    Начальный коммит.

---
+++ src/test/java/ru/gb/antonov/j611/J611ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.antonov.j611;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J611ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
